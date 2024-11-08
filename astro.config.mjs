a3da108d04cd87b73181d343987803296895fde8
David Coe <gitlab@davycoe.com>
2024-11-08 17:38:39 +0000

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import solidJs from '@astrojs/solid-js';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), solidJs(), sitemap()],
+  output: 'server',
+  adapter: cloudflare()
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary1: "#9EB5EE",
+      primary2: "#F2753E",
+      primary3: "#3B7F6C",
+      primary4: "#EEE7CF",
+      status1: "#E35543",
+      status2: "#51BA40",
+      status3: "#F2A141",
+      text1: "#F6F6F6",
+      text2: "#D8D9DB",
+      text3: "#696F76",
+      text4: "#363E48",
+      text5: "#111111",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
